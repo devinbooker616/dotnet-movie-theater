58871cc8c7d8b84fc8110a6ed703750aa6380bb5
Dwayne Bratcher <dbratcher@wvsd.local>
2019-12-05 10:58:20 -0600

    Initialized repository

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace dotnet_movie_theater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
