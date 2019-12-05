a0af0d7a3a27d7c163a9529d8087fb272f982a24
Dwayne Bratcher <dbratcher@wvsd.local>
2019-12-05 11:55:52 -0600

    Created GetMovie method
    the GetMovie method returns the user's desired movie from a selection
    of movies pulled from movies.json

--- Program.cs
+++ Program.cs
@@ -12,8 +12,34 @@ namespace dotnet_movie_theater
             string films = File.ReadAllText(@"./movies.json");
             return JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Movies>>>(films);
         }
+
+        static string GetMovie(Dictionary<string, Dictionary<string, Movies>> movies)
+        {
+            foreach (var movie in movies)
+            {
+                foreach (var film in movie.Value)
+                {
+                    Console.WriteLine(film.Value.Title);
+                }
+            }
+            Console.Write("What movie would you like?: ");
+            string choice = Console.ReadLine();
+            foreach (var movie in movies)
+            {
+                foreach (var film in movie.Value)
+                {
+                    if (film.Value.Title == choice)
+                    {
+                        return choice;
+                    }
+                }
+            }
+            return "Movie not available";
+        }
         static void Main(string[] args)
         {
+            Dictionary<string, Dictionary<string, Movies>> movies = LoadMovies();
+            string choice = GetMovie(movies);
             Console.WriteLine("Hello World!");
         }
     }
