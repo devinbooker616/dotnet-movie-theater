e50939e4664c9e47d4442b89a30e134178c7bfa3
Dwayne Bratcher <dbratcher@wvsd.local>
2019-12-05 11:18:50 -0600

    Created LoadMovies method
    this method will get the entire dictionary from movies.json which can be
    manipulated in Program.cs

--- Program.cs
+++ Program.cs
@@ -7,6 +7,11 @@ namespace dotnet_movie_theater
 {
     class Program
     {
+        static Dictionary<string, Dictionary<string, Movies>> LoadMovies()
+        {
+            string films = File.ReadAllText(@"./movies.json");
+            return JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, Movies>>>(films);
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
